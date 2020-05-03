34b295eebaf6581882f9059dc28cb96c3260c792
Jonathan Marek <Jonathan.marek@mavs.uta.edu>
2020-05-03 10:36:17 -0500

    Started UI design, very rough still. Can select picture to be displayed

--- src/ui/MainWindow.xaml.cs
+++ src/ui/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -23,6 +24,28 @@ namespace Steganography
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // This function is run when the button is clicked. Gets the png file and creates a bitmap
+        private void SelectPicture(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog
+            {
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures),
+                Filter = "Image files (*.png) |*.png",
+                RestoreDirectory = true
+            };
+
+            // Initialize the OpenFileDialog
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string selectedFileName = dlg.FileName;
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(selectedFileName);
+                bitmap.EndInit();
+                stegImage.Source = bitmap;
+            }
         }
     }
 }
